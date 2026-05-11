6b62179dab90d0d851c2ff1f71c4334c4f0d876f
Souradip Roy CHOWDHURY <souradiproychowdhury@gmail.com>
2026-05-11 13:18:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ io.on("connection", (socket)=>{
     });
 });
 
-server.listen(5000, ()=>{
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on http://localhost:5000");
+server.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
 });
